98f1a47b80ddaab2564ec25c478e435ae15aca71
Aliya <aliatokonova36@gmail.com>
2023-04-19 14:08:03 +0600

    Store

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// const main = document.getElementsByName('main');
+//
+// for(let i = 0 ; i <= posts.length; i ++){
+//     main.innerHTML +=
+//         `<div class="post">
+//            <p class="title">${posts[i].title}</p>
+//            <p class="descr">${posts[i].description}</p>
+// </div>
+//
+//         `
+// }
+
+
+
